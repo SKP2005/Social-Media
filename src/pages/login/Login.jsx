33c1a7f72bed37bced0fee41469cc4a708286c8a
Sanjeet <sanjeetkumar6178@gmail.com>
2024-05-05 01:29:50 +0530

    done with ui design with some errors

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./login.scss";
 
 const Login =()=>{
@@ -8,7 +9,9 @@ const Login =()=>{
                     <h1>Hello World.</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet dolores libero officia totam incidunt autem perspiciatis, repellat rerum architecto possimus.</p>
                     <span>Don't you have an account?</span>
-                    <button>Register</button>
+                    <Link to ="/register">
+                        <button>Register</button>
+                    </Link>
                 </div>
                 <div className="right"> 
                 <h1>Login</h1>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./register.scss";
 
 const Register =()=>{
@@ -8,7 +9,9 @@ const Register =()=>{
                     <h1>Social.</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet dolores libero officia totam incidunt autem perspiciatis, repellat rerum architecto possimus.</p>
                     <span>Don't you have an account?</span>
-                    <button>Login</button>
+                    <Link to ="/login">
+                        <button>Login</button>
+                    </Link>
                 </div>
                 <div className="right"> 
                 <h1>Register</h1>
