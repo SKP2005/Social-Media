945f5485cfdcf33518a7b8f6174821188f768d6f
Sanjeet <sanjeetkumar6178@gmail.com>
2024-05-07 00:34:22 +0530

    backend done till before connect.js part

---
+++ api/controllers/auth.js
@@ -0,0 +1,14 @@
+export const register=(req,res)=>{
+    //CHECK USER IF EXISTS
+
+    //CREATE A NEW USER
+    //hash the password
+}
+
+export const login=(req,res)=>{
+
+}
+
+export const logout=(req,res)=>{
+
+}
