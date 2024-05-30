81993b5399435147ad19e1e86261395871d05627
Sanjeet <sanjeetkumar6178@gmail.com>
2024-05-30 18:58:33 +0530

    done until uploading file to server(1.5)

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,83 @@
-import { useContext } from "react";
-import LeftBar from "./components/leftBar/LeftBar";
-import Navbar from "./components/navbar/Navbar";
-import RightBar from "./components/rightBar/RightBar";
-import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import "./style.scss";
 import {
   createBrowserRouter,
-  Navigate,
-  Outlet,
   RouterProvider,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import "./style.scss";
+import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
 
-  const {currentUser}=useContext(AuthContext);
+  const { darkMode } = useContext(DarkModeContext);
 
-  const {darkMode}=useContext(DarkModeContext);
+  const queryClient = new QueryClient();
 
-  const Layout =()=>{
-    return(
-      <div className={`theme-${darkMode? "dark":"light"}`}>
-        <Navbar/>
-        <div style={{display:"flex"}}>
-          <LeftBar/>
-          <div style={{flex:6}}>
-            <Outlet/>
+  const Layout = () => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <div className={`theme-${darkMode ? "dark" : "light"}`}>
+          <Navbar />
+          <div style={{ display: "flex" }}>
+            <LeftBar />
+            <div style={{ flex: 6 }}>
+              <Outlet />
+            </div>
+            <RightBar />
           </div>
-          <RightBar/>
         </div>
-
-      </div>
+      </QueryClientProvider>
     );
   };
 
-  const ProtectedRoute=({children})=>{
-    if(!currentUser){
-      return <Navigate to="/login"/>
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
     }
-    return children
-  }
+
+    return children;
+  };
 
   const router = createBrowserRouter([
     {
-      path:"/",
-      element:<ProtectedRoute>
-          <Layout/>
-        </ProtectedRoute>,
-      children:[
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
         {
-          path:"/",
-          element:<Home/>
+          path: "/",
+          element: <Home />,
         },
         {
-          path:"/profile/:id",
-          element:<Profile/>
-        }
-      ]
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
     },
     {
       path: "/login",
-      element: <Login/>,
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register/>,
+      element: <Register />,
     },
   ]);
+
   return (
     <div>
       <RouterProvider router={router} />
--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
@@ -7,16 +8,12 @@ export const AuthContextProvider = ({ children }) => {
     JSON.parse(localStorage.getItem("user")) || null
   );
 
-  const login = () => {
-    //TO DO
+  const login = async (inputs) => {
+    const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {
+      withCredentials: true,
+    });
 
-    setCurrentUser({
-        id: 1,
-        name: "John Doe",
-        profilePic:
-          "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      });
-    
+    setCurrentUser(res.data)
   };
 
   useEffect(() => {
